eb815e93978051118a9863d8e3d0e4bf35c520ba
lolabussie <Lou.LABUSSIERE@etu.uca.fr>
2023-05-14 19:42:28 +0200

    ajout mapping

---
+++ PinkedIn.Point.Labussiere.Dal/Mapping/EmployeMapping.cs
@@ -0,0 +1,19 @@
+﻿using PinkedIn.Point.Labussiere.Dal.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkedIn.Point.Labussiere.Dal.Mapping
+{
+    public class EmployeMapping : EntityTypeConfiguration<Employe>
+    {
+        public EmployeMapping()
+        {
+            ToTable("APP_EMPLOYE");
+            HasKey(e => e.Id);
+        }
+    }
+}
