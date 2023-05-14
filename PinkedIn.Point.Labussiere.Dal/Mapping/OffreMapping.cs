21613ec01265cf7b660a75681b29b60a0cf440b1
Érina <Erina.POINT@etu.uca.fr>
2023-05-14 18:50:59 +0200

    mapping de l'offre

---
+++ PinkedIn.Point.Labussiere.Dal/Mapping/OffreMapping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkedIn.Point.Labussiere.Dal.Mapping
+{
+    public class OffreMapping : EntityTypeConfiguration<Offre>
+    {
+    }
+}
