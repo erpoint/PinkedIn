ca81fb03eacf8173266528bfb1dc2169aa8d1ee6
Érina <Erina.POINT@etu.uca.fr>
2023-05-27 11:55:50 +0200

    coucouc

--- PinkedIn.Point.Labussiere.Dal/Entity/Offre.cs
+++ PinkedIn.Point.Labussiere.Dal/Entity/Offre.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,15 @@ namespace PinkedIn.Point.Labussiere.Dal.Entity
 {
     public class Offre
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Intitule { get; set; }
         public DateTime Date { get; set; }
         public int Salaire { get; set; }
         public string Description { get; set; }
         public int StatutId { get; set; }
+        public Statut Statut { get; set; }
         public string Responsable { get; set; }
+        public string IdPostulation { get; set; }
+        public Postulation Postulation { get; set; }
     }
 }
