f233a74d51ac3e310fa957525c4837b1368f98d7
Lou LABUSSIERE <lolabussie>
2023-05-27 11:49:48 +0200

    ajout context da

---
+++ PinkedIn.Point.Labussiere.Dal/ContextDA.cs
@@ -0,0 +1,30 @@
+﻿using PinkedIn.Point.Labussiere.Dal.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkedIn.Point.Labussiere.Dal
+{
+    public class ContextDA : DbContext
+    {
+        public ContextDA() : base("name=PinkedInConnectionString") { }
+
+        public DbSet<Employe> Employes { get; set; }
+        public DbSet<Employe> Formations { get; set; }
+        public DbSet<Employe> Experiences { get; set; }
+        public DbSet<Employe> Offres { get; set; }
+        public DbSet<Employe> Postulations { get; set; }
+        public DbSet<Employe> Status { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("dbo");
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
