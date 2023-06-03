9536b8bcf1328d8fdc3ac89eba0c13e75bb2e81c
Érina <Erina.POINT@etu.uca.fr>
2023-06-03 11:40:16 +0200

    repository offre, postulation et statut

---
+++ PinkedIn.Point.Labussiere.BusinessLayer/Repositories/PostulationRepository.cs
@@ -0,0 +1,52 @@
+﻿using PinkedIn.Point.Labussiere.Modele;
+using PinkedIn.Point.Labussiere.Modele.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkedIn.Point.Labussiere.BusinessLayer.Repositories
+{
+    public class PostulationRepository: IRepository<Postulation>
+    {
+        private ContextDA _context;
+        private DbSet<Postulation> _postulations;
+        public PostulationRepository()
+        {
+            this._context = new ContextDA();
+            this._postulations = this._context.Postulations;
+        }
+        public void DeleteEntity(Postulation entity)
+        {
+            _postulations.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public List<Postulation> FindAll()
+        {
+            return _postulations.ToList();
+        }
+
+        public Postulation FindEntity(int id)
+        {
+            return _postulations.Find(id);
+        }
+
+        public void InsertEntity(Postulation entity)
+        {
+            _postulations.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void UpdateEntity(Postulation entity)
+        {
+            var entry = _context.Entry(entity);
+            entry.CurrentValues.SetValues(entity);
+            entry.State = EntityState.Modified;
+
+            _context.SaveChanges();
+        }
+    }
+}
