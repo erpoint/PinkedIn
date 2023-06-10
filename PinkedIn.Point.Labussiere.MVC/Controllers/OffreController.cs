9ce605b4ed4fde9d47c6cecbd8f4d23dad716d41
Lou LABUSSIERE <lolabussie>
2023-06-10 08:43:52 +0200

    ajout projet mvc

---
+++ PinkedIn.Point.Labussiere.MVC/Controllers/OffreController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PinkedIn.Point.Labussiere.MVC.Controllers
+{
+    public class OffreController : Controller
+    {
+        // GET: Offre
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Offre/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Offre/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Offre/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Offre/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Offre/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Offre/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Offre/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PinkedIn.Point.Labussiere.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PinkedIn.Point.Labussiere.MVC.MvcApplication" Language="C#" %>
