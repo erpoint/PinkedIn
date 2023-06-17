40b7b41a17068301eb82a6ed885afe13b75281fc
Érina <Erina.POINT@etu.uca.fr>
2023-06-17 09:48:27 +0200

    OK ca marche pas

---
+++ PinkdIn.Point.Labussiere.gui/ViewModel/OffreVM.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkdIn.Point.Labussiere.gui.ViewModel
+{
+    public class OffreVM : BaseViewModel
+    {
+        private int id;
+
+        private string intitule;
+
+        private DateTime date;
+
+        private int salaire;
+
+        private string description;
+
+        private int statutId;
+
+        private StatutVM statut;
+
+        private string responsable;
+
+        private string idPostulation;
+
+        //private List<PostulationVM> postulation;
+
+        public int Id {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+
+        public string Intitule {
+            get
+            {
+                return intitule;
+            }
+            set
+            {
+                intitule = value;
+                OnPropertyChanged(nameof(Intitule));
+            } 
+        }
+
+        public DateTime Date {
+            get { return date; }
+            set
+            {
+                date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+
+        public int Salaire {
+            get { return salaire; }
+            set { 
+                salaire = value;
+                OnPropertyChanged(nameof(Salaire));
+            }
+        }
+
+        public string Description { 
+            get { return description; }
+            set { 
+                description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public StatutVM Statut {
+            get { return statut; }
+            set { 
+                statut = value;
+                OnPropertyChanged(nameof(Statut));
+            }
+        }
+
+        public string Responsable { get; set; }
+
+        public string IdPostulation { get; set; }
+
+        //public List<PostulationVM> Postulation { get; set; }
+    }
+}
---
+++ PinkdIn.Point.Labussiere.gui/ViewModel/StatutVM.cs
@@ -0,0 +1,45 @@
+﻿using PinkedIn.Point.Labussiere.Modele.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkdIn.Point.Labussiere.gui.ViewModel
+{
+    public class StatutVM: BaseViewModel
+    {
+        private int id;
+        private string libelle;
+        private Statut statut;
+
+        public StatutVM(Statut statut)
+        {
+            this.statut = statut;
+            this.id = statut.Id;
+            this.libelle = statut.Libelle;
+        }
+
+        /// <summary>
+        /// Identifiant du statut.
+        /// </summary>
+        public int Id { get { return id; } set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            } }
+
+        /// <summary>
+        /// Libellé du statut.
+        /// </summary>
+        public string Libelle
+        {
+            get { return libelle; }
+            set
+            {
+                libelle = value;
+                OnPropertyChanged(nameof(Libelle));
+            }
+        }
+    }
+}
